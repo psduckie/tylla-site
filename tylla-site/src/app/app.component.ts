bb4f8b721ab5296a0715fb28600347f489a58db8
Tyll'a <psduckie@yahoo.com>
2019-06-03 18:01:01 -0500

    Removed unneeded imports.

--- tylla-site/src/app/app.component.ts
+++ tylla-site/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { MarkdownService } from 'ngx-markdown';
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-root',
