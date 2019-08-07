763cc079f9d1ab98e55eec74bd8f47c324e87419
Tyll'a <psduckie@yahoo.com>
2019-08-06 23:51:29 -0500

    Connected main navbar to index and storyline pages

---
+++ js/pageloads.js
@@ -0,0 +1,12 @@
+function load_navbar() {
+  document.getElementById("navbar").innerHTML =
+    '<object type="text/html" data="navbar.html" style="width: 100%"></object>';
+}
+
+function load_homepage() {
+  load_navbar();
+}
+
+function load_storylines() {
+  load_navbar();
+}
