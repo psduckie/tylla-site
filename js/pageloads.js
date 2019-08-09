8ac60c98478ef45f6c9454074eb2530d0ac50496
Tyll'a <psduckie@yahoo.com>
2019-08-09 13:33:04 -0400

    Worked on the method for fetching files.

--- js/pageloads.js
+++ js/pageloads.js
@@ -1,3 +1,19 @@
+function pull_file(path) {
+//  alert(typeof path);
+  if(typeof path != "string") {
+//    alert("Not a string");
+    return "Error: Invalid path.  Path must be a string.";
+  }
+//  alert("A string");
+  fetch(path)
+  .then(function() {
+    alert(path & " passed.");
+  })
+  .catch(function() {
+    alert(path & " failed.");
+  })
+}
+
 function load_navbar() {
   document.getElementById("navbar").innerHTML =
     '<object type="text/html" data="navbar.html" style="width: 100%"></object>';
