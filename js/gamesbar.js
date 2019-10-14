f110c9d985b5b19893f1c077a405aa0146f2870f
Tyll'a <psduckie@yahoo.com>
2019-10-14 16:38:13 -0400

    Fixed an error in the gamesbar code

--- js/gamesbar.js
+++ js/gamesbar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log(screen.width);
-    if(screen.width < 576) {
+    if(screen.width >= 576) {
         $("#gamesbar").load("/storylines/gamesbar.html");
     }
 });
