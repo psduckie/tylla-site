47380c91145095e1dada5087983ded487e212e39
Tyll'a <psduckie@yahoo.com>
2019-10-17 17:31:06 -0400

    Added JQuery to adjust headers

---
+++ js/headers.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    if(screen.width >= 576) {
+        $("h1.responsiveHeader").addClass("display-1");
+        $("h2.responsiveHeader").addClass("display-4");
+    }
+});
